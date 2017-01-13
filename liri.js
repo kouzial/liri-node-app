ac1335d5e0af3611e5325a9105f9c472f39e9e55
Khaled ouziala <khaledouziala@Khaleds-MacBook-Pro-2.local>
2017-01-12 19:53:02 -0600

    l

---
+++ liri.js
@@ -0,0 +1,9 @@
+// Require access to all of the exports in our key.js
+
+var access = require('/key.js');
+
+// Print data that is in the exports
+
+console.log(access.twitterKeys);
+
+// If statement from Calculator.js
