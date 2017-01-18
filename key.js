424ae7dfaa792dd7995d3646d20c394afaea9ecb
Khaled ouziala <khaledouziala@Khaleds-MacBook-Pro-2.local>
2017-01-18 17:35:22 -0600

    Final

--- key.js
+++ key.js
@@ -2,9 +2,10 @@ console.log('this is loaded');
 
 exports.twitterKeys = {
 
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: '',
+	consumer_key: 'PW39mYuTQHEEwriQlcqCDgzXe',
+	consumer_secret: 'LyjG621injeXc6J6QTzgDECmycU0fRx2JiluROg3qSFWPg24Ud',
+	access_token_key: '789994846056427521-cf7l1lqNKQjrZJlIRouUYKEPo4Aag2S',
+	access_token_secret: 'dhnA7jj6z51R2bQQXFVuo1x52Lk231jWlRuOBYDj8sJth',
 
 }
+
--- liri.js
+++ liri.js
@@ -1,9 +1,80 @@
 // Require access to all of the exports in our key.js
 
-var access = require('/key.js');
+var access = require('./key.js');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
 
 // Print data that is in the exports
 
 console.log(access.twitterKeys);
 
-// If statement from Calculator.js
+// Takes in all of the command line arguments
+var inputString = process.argv;
+
+// Parses the command line argument to capture the "operand" (my-tweets, spotify-this-song, movie-this, do-what-it-says) and the data
+var operand = inputString[2];
+var data = inputString[3];
+
+
+
+// if statement
+
+if (operand === "my-tweets") {
+  var client = new Twitter(access.twitterKeys);
+  client.get(
+  	'statuses/home_timeline',
+  	function(error, tweets, reponse) {
+  		for (var i=0; i < tweets.length; i++) {
+  			console.log(tweets[i].text + " " + tweets[i].created_at);  		
+  		}
+  	}
+  )
+}
+
+else if (operand === "spotify-this-song") {
+
+	spotify.search(
+		{
+			type:'track',
+			query:data === undefined ? "the sign" : data
+		},
+		function(err, data) {
+			if (err) {
+				console.log('Error occured: '+ err);
+				return;
+			}
+			data.tracks.items.map(
+				function(a) {
+					console.log("Artist: " + a.artists[0].name + ", Song: " + a.name + ", Album: " + a.album.name + ", Link: " +a.external_urls.spotify);
+					return;
+				}
+			)
+		}
+
+	)
+
+}
+
+else if (operand === "movie-this") {
+
+
+
+
+}
+
+else if (operand === "do-what-it-says") {
+
+var fs = require("fs");
+
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+
+  // We will then print the contents of data
+  console.log(data);
+
+
+  
+});
+
+}
